71127c00cb6f84b43a9cddb3ab62d8440869f8eb
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-05-03 23:23:00 +0100

    write into a file

---
+++ assignment.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const https = require('https');
+
+const filePath = './public/Tunde_onakoya.html';
+let responseData = '';
+
+https.get('https://en.wikipedia.org/wiki/Tunde_Onakoya', (response) => {
+    console.log('statusCode:', response.statusCode);
+    console.log('headers:', response.headers);
+
+    response.on('data', (data) => {
+        responseData += data;
+    });
+
+    response.on('end', () => {
+        fs.writeFile(filePath, responseData, (err) => {
+            if (err) {
+                console.error('Error writing file:', err);
+            } else {
+                console.log('File has been saved!');
+            }
+        });
+    });
+}).on('error', (error) => {
+    console.error('Error making request:', error);
+});
